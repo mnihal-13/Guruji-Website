17e7ba3b445eaa0df550be22cfde5d24fc014146
Mohammed Nihal <mnkv1013@gmail.com>
2025-05-10 16:39:08 +0530

    updates

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -95,17 +95,17 @@ Version: 1.0
     nav:true,
 	autoplay:true,
 	items: 2,
-	autoplayTimeout: 2000,
+	autoplayTimeout: 3000,
 	// autoplayHoverPause: true,
     responsive:{
         0:{
             items:1
         },
-        600:{
+        768:{
             items:1
         },
-        1000:{
-            items:1
+        1200:{
+            items:2
         }
     }
     });
